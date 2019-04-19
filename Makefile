4dd1459b3d6accf73d6dffdb6473a42e533a3ff1
Safe.net <safe.net@Apple-Mac.local>
2019-04-19 17:24:24 +0200

    Run functionality incomplete in MakeFile

--- Makefile
+++ Makefile
@@ -1,20 +1,15 @@
 JFLAGS = -g
 JC = javac
-
+J = java
+SOURCES = src/BarrierS/*.java
+CLASSES = src/BarrierS/*.classes
 .SUFFIXES: .java .class
 
-.java.class:
-	$(JC) $(JFLAGS) 
+default:
+	$(JC) $(JFLAGS) $(SOURCES)
 
-CLASSES = \
-		/src/Barrier.java \
-		/src/BarrierTest.java \
-		/src/BThread.java
-
-default: classes
-
-classes: $(CLASSES: .java = .class)
-
+run:
+	$(J) src/BarrierS/BarrierTest.class
 clean:
-	cp *.class bin/
-	rm *.class
+	cp *.class ../bin
+	rm $(CLASSES)
--- src/BarrierS/Barrier.java
+++ src/BarrierS/Barrier.java
@@ -4,16 +4,24 @@ import java.util.concurrent.Semaphore;
 
 public class Barrier {
 
-	// add missing variables
 	private int limit;
 	private final Semaphore semaphore;
 
 	Barrier(int limit) {
 		this.semaphore = new Semaphore(limit); // Create Semaphore and set limit
+		this.limit = limit; // Set limit as input limit
+	}
+
+	public int getLimit() {
+		return limit;
 	}
 
 	public void b_wait() throws InterruptedException {
-		semaphore.acquire();
+		int counter = 0;
+		while (counter < this.limit) {
+			semaphore.acquire();
+		}
+
 	}
 
 }
